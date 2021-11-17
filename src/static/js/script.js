404439beabb15f5ca7c1c6f5f4259290e1d9387a
Mariana Tamay <maritamay4@gmail.com>
2021-11-17 11:29:47 -0300

    Ajuste da responsividade e botões

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -75,11 +75,3 @@ function mostrar() {
   }
 }
 
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
