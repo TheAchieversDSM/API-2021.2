f77bdc7448cdf3bec5c5506eef3c711dc3f46c60
Matheus.Sakuragui <matheus.lemesakuragui@gmail.com>
2021-11-25 17:59:36 -0300

    Anexando multiplos arquivos(Falta função de Download)

--- src/app.py
+++ src/app.py
@@ -17,7 +17,7 @@ app.config['SECRET_KEY'] = 'TheAchieversDSM'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '20210618'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'fatec_api'
 
 mysql = MySQL(app)
